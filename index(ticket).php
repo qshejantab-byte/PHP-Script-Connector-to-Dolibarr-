7ed42ed0fd4bc95d62452c04fcda531d33222987
qshejantab-byte <qshejantab@gmail.com>
2026-03-13 11:35:55 +0200

    Add files via upload

---
+++ index(ticket).php
@@ -0,0 +1,60 @@
+<?php
+
+$url = "https://alphacomputer.rw/dolibarr/htdocs/api/index.php/ticket";
+
+
+
+$data = [
+    "name" => $_POST['name'],
+    "email" => $_POST['email'],
+    "phone" => $_POST['phone'],
+    "client" => 2
+];
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "DOLAPIKEY: Ny5N20J9igXON01dBu2wxz6b4ITa7pEM",
+    "Content-Type: application/json"
+]);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo "Ticket created successfully";
+
+____________________________________________________________
+
+$url = "https://alphacomputer.rw/dolibarr/htdocs/api/index.php/projects/list of open leads";
+
+
+
+$data = [
+    "name" => $_POST['name'],
+    "email" => $_POST['email'],
+    "phone" => $_POST['phone'],
+    "client" => 2
+];
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "DOLAPIKEY: Ny5N20J9igXON01dBu2wxz6b4ITa7pEM",
+    "Content-Type: application/json"
+]);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo "Lead created successfully";
+
+
+?>
